efe987820e084f6508619a77621ce1985e4fc523
christianPearson <cpearson@mutualser.org>
2023-06-28 20:06:37 -0500

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {MatTableModule} from "@angular/material/table";
+import {HttpClient} from "@angular/common/http";
+export interface PeriodicElement {
+  identificacion: string;
+  nombres: string;
+  apellidos: string;
+  tipocontrato: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
   title = 'taller-frontend';
+
+  displayedColumns: string[] = ['identificacion', 'nombres', 'apellidos', 'tipocontrato'];
+  dataSource: Array<PeriodicElement> = [];
+
+
+  ngOnInit(): void {
+     this.http.get<any>('http://localhost:3000/docentes').subscribe(value => {
+       console.log(value);
+       this.dataSource = value;
+     });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {MatTableModule} from "@angular/material/table";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from "@angular/common/http";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
